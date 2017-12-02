20adcdbae339be1b77bc4b8278f58c87dc17d9d6
lipd <lipeidao@gmail.com>
2017-12-02 20:55:22 +0800

    encapsulate Tabs

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import { StyleSheet, Text, View, StatusBar, ScrollView } from 'react-native'
-import { TabNavigator, StackNavigator } from 'react-navigation'
+import { View, StatusBar } from 'react-native'
 import { Constants } from 'expo'
-import {
-  Ionicons,
-  Foundation,
-  MaterialCommunityIcons,
-  Entypo
-} from '@expo/vector-icons'
 import { white, yellow } from './utils/colors'
+import Tabs from './views/Tabs'
 import MusicLibrary from './views/MusicLibrary'
+import Mine from './views/Mine'
 
 function MyStatusBar({ backgroundColor, ...props }) {
   return (
@@ -18,61 +13,6 @@ function MyStatusBar({ backgroundColor, ...props }) {
     </View>
   )
 }
-
-const Tabs = TabNavigator(
-  {
-    MusicView: {
-      screen: MusicLibrary,
-      navigationOptions: {
-        tabBarLabel: '音乐库',
-        tabBarIcon: () => (
-          <MaterialCommunityIcons
-            name="music-circle"
-            size={30}
-            color={yellow}
-          />
-        )
-      }
-    },
-    PostView: {
-      screen: View,
-      navigationOptions: {
-        tabBarLabel: '文章',
-        tabBarIcon: () => <Ionicons name="md-book" size={30} color={yellow} />
-      }
-    },
-    View: {
-      screen: View,
-      navigationOptions: {
-        tabBarLabel: '音乐资讯',
-        tabBarIcon: () => <Entypo name="documents" size={30} color={yellow} />
-      }
-    },
-    NewDeckView: {
-      screen: View,
-      navigationOptions: {
-        tabBarLabel: '圈子',
-        tabBarIcon: () => (
-          <Entypo name="circular-graph" size={30} color={yellow} />
-        )
-      }
-    },
-    DeckView: {
-      screen: View,
-      navigationOptions: {
-        tabBarLabel: '个人主页',
-        tabBarIcon: () => (
-          <MaterialCommunityIcons name="home-circle" size={30} color={yellow} />
-        )
-      }
-    }
-  },
-  {
-    tabBarOptions: {
-      activeTintColor: '#ffb630'
-    }
-  }
-)
 
 export default class App extends React.Component {
   render() {
---
+++ views/Tabs.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import {
+  Ionicons,
+  Foundation,
+  MaterialCommunityIcons,
+  Entypo
+} from '@expo/vector-icons'
+import { StyleSheet, Text, View, StatusBar, ScrollView } from 'react-native'
+import { TabNavigator } from 'react-navigation'
+import { Constants } from 'expo'
+import MusicLibrary from './MusicLibrary'
+import Mine from './Mine'
+import { white, yellow } from '../utils/colors'
+
+const Tabs = TabNavigator(
+  {
+    MusicLibrary: {
+      screen: MusicLibrary,
+      navigationOptions: {
+        tabBarLabel: '音乐库',
+        tabBarIcon: () => (
+          <MaterialCommunityIcons
+            name="music-circle"
+            size={30}
+            color={yellow}
+          />
+        )
+      }
+    },
+    PostView: {
+      screen: View,
+      navigationOptions: {
+        tabBarLabel: '文章',
+        tabBarIcon: () => <Ionicons name="md-book" size={30} color={yellow} />
+      }
+    },
+    View: {
+      screen: View,
+      navigationOptions: {
+        tabBarLabel: '音乐资讯',
+        tabBarIcon: () => <Entypo name="documents" size={30} color={yellow} />
+      }
+    },
+    NewDeckView: {
+      screen: View,
+      navigationOptions: {
+        tabBarLabel: '圈子',
+        tabBarIcon: () => (
+          <Entypo name="circular-graph" size={30} color={yellow} />
+        )
+      }
+    },
+    Mine: {
+      screen: Mine,
+      navigationOptions: {
+        tabBarLabel: '个人主页',
+        tabBarIcon: () => (
+          <MaterialCommunityIcons name="home-circle" size={30} color={yellow} />
+        )
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: '#ffb630'
+    }
+  }
+)
+
+export default Tabs
